fea57479a24cab7babede70e7368e7d06994a103
Bernd Schoolmann <quextenmail@googlemail.com>
2018-03-01 14:04:10 +0100

    Add initial version

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <v-content>
+      <router-view/>
+    </v-content>
+    <v-footer :fixed="fixed" app>
+      <span>Code by</span>
+      <v-btn flat color="primary" @click.native="visitQuexten()">Quexten</v-btn>
+      <span>Hosting and PR by</span>
+      <v-btn flat color="primary" @click.native="visitMarnit()">Marnit</v-btn>
+      <v-spacer></v-spacer>
+      <span>With 💖 on</span>
+      <v-btn flat color="primary" @click.native="visitGithub()">GitHub</v-btn>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  methods: {
+    visitMarnit: function () {
+      window.location.href = 'https://github.com/themarnit'
+    },
+    visitQuexten: function () {
+      window.location.href = 'https://github.com/quexten'
+    },
+    visitGithub: function () {
+      window.location.href = 'https://github.com/parkitectattac/'
+    }
+  }
+}
+</script>
+
---
+++ src/components/Main.vue
@@ -0,0 +1,76 @@
+<template>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4>
+            <v-card class="elevation-12">
+              <v-toolbar dark color="primary">
+              <v-toolbar-title>Parkitect Mod Portal</v-toolbar-title>
+                <v-spacer></v-spacer>
+              </v-toolbar>
+              <v-content>
+                <v-card-text>
+                  <v-text-field v-model="link" color="primary" label = "Workshop Link"></v-text-field>
+                </v-card-text>
+                <v-btn
+                  color="accent"
+                  :loading="downloading"
+                  @click.native="download(link)"
+                  :disabled="downloading"                >
+                  Download
+                </v-btn>
+              </v-content>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    data () {
+      return {
+        link: '',
+        downloading: false
+      }
+    },
+    methods: {
+      download: function (workshoplink) {
+        var self = this
+        self.downloading = true
+
+        var itemId = workshoplink.match(/\d+$/)
+        axios.get('api-parkitect-marnit.dynv6.net', {
+          item_id: itemId
+        })
+        .then(function (response) {
+          window.location.href = response.data.download
+          self.downloading = false
+        })
+        .catch(function (error) {
+          self.error = error
+          self.downloading = false
+        })
+      }
+    }
+  }
+</script>
